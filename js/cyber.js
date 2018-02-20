59b7da273b3e10a700b1418fc2c77f404865c133
Haim <shlez1993.hs@gmail.com>
2018-02-20 19:54:21 +0200

    Updated Simle XSS level

---
+++ js/cyber.js
@@ -0,0 +1,27 @@
+
+
+function showHint() { $('#hint-body').show(); }
+
+function hideHint() { $('#hint-body').hide(); }
+
+function callMe() { window.location.href = "./cyber3.html"; }
+
+function checkTime(i) {
+  if (i < 10) {
+    i = "0" + i;
+  }
+  return i;
+}
+
+function getTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+
+    // add a zero in front of numbers<10
+    m = checkTime(m);
+    s = checkTime(s);
+    var time = h + ':' + m + ':' + s;
+    $('#time').text(time);
+}
